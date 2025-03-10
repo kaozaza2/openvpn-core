8dc8701066254cbda1c2a3996fbf26b3de805c44
Kao Kittisak <kittisakphiri@gmail.com>
2025-03-10 15:14:32 +0700

    remove unused file

--- library/src/main/java/net/openvpn/openvpn/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/openvpn3/client
-# Build directory: /home/ubuntu/openvpn3/build-android/client
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
